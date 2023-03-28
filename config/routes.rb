20e97c925521b1bbaa5f4e890bdb4364c26e7bfd
Sammy-CK <sammycherono2696@gmail.com>
2023-03-28 14:33:21 +0300

    Adddded user endpoints and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
   resources :genres
   # resources :book_genres
-  resources :books [:show, :update :destroy]
+  resources :books, only: [:show, :update, :destroy]
   resources :reviews
-  resources :users
+  # resources :users, only: [:]
+
+
+# user
+  get '/users', to: 'users#create'
+  post '/users/login', to: 'users#login'
+  get '/users/logout', to: 'users#logout'
+  get '/users/checklogin', to: 'users#check_logged_in'
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
