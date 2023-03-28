af47574d1f0bcd956a8ac6707fbd0b090bf64dae
shuaib shaban <shuaib>
2023-03-28 12:48:47 +0300

    session code added in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
+rescue_from ActiveRecord::StandardError, with: :standard_error_finder
+
+    def user
+        user = User.find(session[:uid])
+    end
+
+    def standard_error_finder
+        render json: {errors: invalid.errors}, status: :400
+    end
 end
