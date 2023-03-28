0c4132f280d53e57ab24d8e044f2d4e944a2152a
Samueelx <gsammygitau@gmail.com>
2023-03-28 14:32:30 +0300

    init.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :genres
   # resources :book_genres
-  resources :books
+  resources :books [:show, :update :destroy]
   resources :reviews
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
