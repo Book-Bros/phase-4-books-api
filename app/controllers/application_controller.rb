ffc895605cd00ed868723eabfdbc37427ea006cc
shuaib shaban <shuaib>
2023-03-29 09:19:41 +0300

    books controller, routes, model and serializer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
-rescue_from ActiveRecord::StandardError, with: :standard_error_finder
+# rescue_from ActiveRecord::StandardError, with: :standard_error_finder
 
     def user
         User.find(session[:uid])
@@ -8,7 +8,7 @@ rescue_from ActiveRecord::StandardError, with: :standard_error_finder
 
     private
 
-    def standard_error_finder
-        render json: {errors: invalid.errors}, status: :400
-    end
+    # def standard_error_finder
+    #     render json: {errors: invalid.errors}, status: :400
+    # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :genres
   # resources :book_genres
-  resources :books [:show, :update :destroy]
+  resources :books
   resources :reviews
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
