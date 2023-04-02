2a03c5644812e058f9871a32548e9875471730d8
Sammy-CK <sammycherono2696@gmail.com>
2023-04-02 23:35:18 +0300

    added session store secure

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,8 @@ module Phase4BooksApi
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
 
-    config.action_dispatch.cookies_same_site_protection = :lax
+    config.action_dispatch.cookies_same_site_protection = :none
+    config.session_store :cookie_store, key: '_your_app_session', secure: true, same_site: :none
+
   end
 end
